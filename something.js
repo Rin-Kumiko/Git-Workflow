7e92507b26677883eac84b74b2699b46b01c20d3
Rin-Kumiko <ayayauwu3@gmail.com>
2024-05-14 10:59:20 +0200

    Replace hard-coded values in loop with variables 'min' and 'max'

--- something.js
+++ something.js
@@ -1,6 +1,7 @@
 // Some useless JavaScript code
-let min = 5;
-let max = 25;
-for (let index = min; index < max; index++) {
-    console.log(index);
+let min = 0; // Valeur minimale
+let max = 10; // Valeur maximale
+
+for (let i = min; i < max; i++) {
+    console.log(i);
 }
